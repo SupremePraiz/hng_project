aa01f10657440a572c66ab7240b3935018716cfb
Jude Adeyemi <supremepraiz2@gmail.com>
2025-01-30 18:50:38 +0100

    made changes to urls

--- account/api/urls.py
+++ account/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . views import ProfileListView
 
 urlpatterns = [
-    path("profile/", ProfileListView.as_view(), name="profile-list"),
+    path("", ProfileListView.as_view(), name="profile-list"),
 ]
 
 
