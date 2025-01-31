50a1d3729490fde754ecd090fef290f4409fe494
Jude Adeyemi <supremepraiz2@gmail.com>
2025-01-31 09:41:27 +0100

    Added an Allowed Host

--- dataproject/settings.py
+++ dataproject/settings.py
@@ -131,6 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Allow all origins
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+    "https://hng-api-0-test.netlify.app/",
+]
 
 
 
